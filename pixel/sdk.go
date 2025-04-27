125b52b672b98dfb273c0a8aa94b3f2f75666fab
Cameron <cwbarclift@gmail.com>
2025-04-26 22:42:58 -0700

    multi-dice in theory, background scanning (#1)
    Co-authored-by: Cameron Barclift <cwbarclift@husky.home>

--- pixel/sdk.go
+++ pixel/sdk.go
@@ -2,18 +2,21 @@ package pixel
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 	"time"
 	"tinygo.org/x/bluetooth"
 )
+
+var pixelServiceUuid, _ = bluetooth.ParseUUID(PixelsService)
+var notifyCharacterUuid, _ = bluetooth.ParseUUID(PixelNotifyCharacteristic)
+var writeCharacteristicUUid, _ = bluetooth.ParseUUID(PixelWriteCharacteristic)
 
 type Die struct {
 	device           bluetooth.Device
 	writeChar        bluetooth.DeviceCharacteristic
 	notifyChar       bluetooth.DeviceCharacteristic
 	ledCount         uint8
-	pixelId          uint32
+	PixelId          uint32
 	CurrentFaceIndex uint8
 	CurrentFaceValue uint8
 	rollState        uint8
@@ -21,18 +24,91 @@ type Die struct {
 	batteryCharging  bool
 	buildTimestamp   uint32
 	designAndColor   uint8
-	LastUpdated      time.Time
+	LastRolled       time.Time
 }
 
-func rgbaToUint32(c color.RGBA) uint32 {
-	return uint32(c.R)<<24 | uint32(c.G)<<16 | uint32(c.B)<<8 | uint32(c.A)
+func WatchForDice(adapter *bluetooth.Adapter, dieChan chan<- *Die) {
+	seenPixelDice := make(map[string]bool)
+	adapter.SetConnectHandler(func(device bluetooth.Device, connected bool) {
+		seenPixelDice[device.Address.String()] = connected
+		fmt.Printf("Device %s status: %v\n", device.Address, connected)
+	})
+
+	for {
+		devCh := make(chan bluetooth.ScanResult, 1)
+		err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+			if !device.HasServiceUUID(pixelServiceUuid) {
+				return
+			}
+			connected := seenPixelDice[device.Address.String()]
+			println(fmt.Sprintf("scanned: %v", device.Address))
+			if !connected {
+				println(fmt.Sprintf("Found device: %s", device.Address))
+				devCh <- device
+				adapter.StopScan()
+				//time.Sleep(100 * time.Millisecond)
+			}
+		})
+		if err != nil {
+			println(fmt.Errorf("scan failed: %v", err))
+		}
+
+		device := <-devCh
+		connected := seenPixelDice[device.Address.String()]
+		//println(fmt.Sprintf("connected: %v", connected))
+		if connected {
+			continue
+		}
+		result, err := adapter.Connect(device.Address, bluetooth.ConnectionParams{})
+		if err != nil {
+			println(fmt.Errorf("connection failed: %v", err))
+			return
+		}
+		die, _ := ConnectDev(&result, 5*time.Second)
+		dieChan <- die
+
+		//println(fmt.Sprintf("Connect device: %s", result.Address))
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func ConnectDev(device *bluetooth.Device, timeout time.Duration) (*Die, error) {
+	var die Die
+	die.device = *device
+
+	services, err := device.DiscoverServices([]bluetooth.UUID{pixelServiceUuid})
+	if err != nil {
+		return nil, fmt.Errorf("service discovery failed: %v", err)
+	}
+
+	for _, service := range services {
+		if service.UUID().String() != PixelsService {
+			continue
+		}
+
+		chars, _ := service.DiscoverCharacteristics([]bluetooth.UUID{notifyCharacterUuid, writeCharacteristicUUid})
+		for _, char := range chars {
+			if char.UUID().String() == PixelNotifyCharacteristic {
+				die.notifyChar = char
+				err = die.notifyChar.EnableNotifications(die.PixelCharacteristicReceiver)
+			} else if char.UUID().String() == PixelWriteCharacteristic {
+				die.writeChar = char
+			}
+		}
+	}
+
+	_ = die.SendMsg(MessageWhoAreYou{})
+	end := time.Now().Add(timeout)
+	for time.Now().Before(end) {
+		if die.PixelId != 0 {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return &die, nil
 }
 
 func (die *Die) Connect(adapter *bluetooth.Adapter) error {
-	pixelServiceUuid, _ := bluetooth.ParseUUID(PixelsService)
-	notifyCharacterUuid, _ := bluetooth.ParseUUID(PixelNotifyCharacteristic)
-	writeCharacteristicUUid, _ := bluetooth.ParseUUID(PixelWriteCharacteristic)
-
 	ch := make(chan bluetooth.ScanResult, 1)
 	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 
@@ -96,7 +172,7 @@ func (die *Die) PixelCharacteristicReceiver(buf []byte) {
 		if msg.RollState == RollStateOnFace || msg.RollState == RollStateRolled {
 			die.CurrentFaceIndex = msg.CurrentFaceIndex
 			die.CurrentFaceValue = msg.CurrentFaceValue
-			die.LastUpdated = time.Now()
+			die.LastRolled = time.Now()
 		}
 	case MsgTypeBlinkAck:
 		log.Printf("Blink Ack: %x", buf)
